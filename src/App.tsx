379b71130e456e368a6b5f30c445c4fa42ab3b11
Nagul75 <nagulk697@gmail.com>
2026-02-23 09:42:00 +0530

    Test inset sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { ComponentExample } from "@/components/component-example";
-
+import Layout from "./components/Layout";
 export function App() {
-return <ComponentExample />;
+  return (
+    <Layout>
+      {/* simulate tall content */}
+      <div className="space-y-4">
+        {Array.from({ length: 50 }).map((_, i) => (
+          <div key={i} className="h-18 p-2 rounded-md border w-1/2">
+            Item {i + 1}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
